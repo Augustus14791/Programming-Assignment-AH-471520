0209f6ce028e7953c827b2f7b3a5f990ddbc9414
Augustus14791 <gus.housley.520@cranfield.ac.uk>
2025-06-12 10:10:40 +0000

    Start

---
+++ main.py
@@ -0,0 +1,13 @@
+def Welcome ():
+    print('Welcome trusted servant of the state')
+    choice1=int(input('Choose your title:\n1) Lord Garry\n2) Lady Janet\n3) Sir Derek IV'))
+    match choice1 
+    case 1:
+    print (1)
+    case 2:
+    print (2)
+    case 3:
+    print (3)
+    else:
+        Welcome()
+        
