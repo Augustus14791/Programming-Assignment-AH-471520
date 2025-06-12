c918797dcbb78bdaa06a1dbd6f99d38a9f5bce18
Augustus14791 <gus.housley.520@cranfield.ac.uk>
2025-06-12 15:58:58 +0000

    Update

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-def Welcome ():
-    print('Welcome trusted servant of the state')
-    choice1=int(input('Choose your title:\n1) Lord Garry\n2) Lady Janet\n3) Sir Derek IV'))
-    match choice1 
-    case 1:
-    print (1)
-    case 2:
-    print (2)
-    case 3:
-    print (3)
-    else:
-        Welcome()
-        
+def Welcome():
+    print('Welcome trusted servant of the state!\n')
+    try:
+        choice1 = int(input('Choose your title:\n1) Lord Garry\n2) Lady Janet\n3) Sir Derek IV\n'))
+    except ValueError:
+        print("Please enter a number.")
+        return Welcome()
+
+    match choice1:
+        case 1:
+            print("Greetings Lord Garry!")
+        case 2:
+            print("Greetings Lady Janet!")
+        case 3:
+            print("Greetings Sir Derek IV!")
+        case _:
+            print("Invalid choice. Try again")
+            Welcome()
+
+Welcome()
+
