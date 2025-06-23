66d12ecae4a2f918620d71eb7120b64fd00a089f
Augustus14791 <gus.housley.520@cranfield.ac.uk>
2025-06-23 18:11:31 +0000

    Copilot utilised to correct syntax errors - incorrect indentation etc

--- main.py
+++ main.py
@@ -26,21 +26,25 @@ def Challenge1(title):
     print('The people of Barrowlandia are facing an economic depression.')
     print('The people are hopeless and depressed!')
     print('We have 3 choices, choose wisely as the people are relying on you.')
-try:
-        choice2 = int(input('Choose your title:\n1) Place funding into industry to support jobs\n2) Provide cheap beer to raise spirits\n3) Increase state benefits and universal credit to improve the standard of living\n'))
+
+    try:
+        choice2 = int(input('Choose your strategy:\n1) Place funding into industry to support jobs\n2) Provide cheap beer to raise spirits\n3) Increase state benefits and universal credit to improve the standard of living\n'))
     except ValueError:
         print("Please enter a number.")
-        return Challenge1() 
-match choice2:
+        return Challenge1(title)
+
+    match choice2:
         case 1:
-            title = "Fantastic choice. The people are starting to realise that there are opportunities out there \nAlthough there is still work to do, spirits are still low"
+            print("Fantastic choice. The people are starting to realise that there are opportunities out there.\nAlthough there is still work to do, spirits are still low.")
         case 2:
-            title = "Well what a nice idea!!\nUnfortunately, the cheap booze caused a riot at the rugby\nBUT spirits are still high!"
+            print("Well what a nice idea!!\nUnfortunately, the cheap booze caused a riot at the rugby.\n150 people died\nBUT spirits are still high! (Pun Intended)")
         case 3:
-            title = "Whatever are we paying you for?\nThe people have given up any kind of employment and becoming couch potatoes"
+            print("Whatever are we paying you for?\nThe people have given up any kind of employment and are becoming couch potatoes.")
         case _:
             print("Invalid choice you gremlin. Try again")
-            return Challenge1()
+            return Challenge1(title)
+
+Welcome()
 
 
 
