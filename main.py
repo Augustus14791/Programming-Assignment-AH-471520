7d890ed2af497b496e389d4262b88be62e5aaf82
Augustus14791 <gus.housley.520@cranfield.ac.uk>
2025-06-23 18:30:55 +0000

    Copied and pasted challenge1 down and now adapting the code to repeat step. Will use MS Copilot to generate a scenario

--- main.py
+++ main.py
@@ -44,7 +44,29 @@ def Challenge1(title):
             print("Invalid choice you gremlin. Try again")
             return Challenge1(title)
 
-Welcome()
+def challenge2
+def Challenge1(title):
+    print(f'\nIt is time for your first challenge, {title}!')
+    print('The people of Barrowlandia are facing an economic depression.')
+    print('The people are hopeless and depressed!')
+    print('We have 3 choices, choose wisely as the people are relying on you.')
+
+    try:
+        choice2 = int(input('Choose your strategy:\n1) Place funding into industry to support jobs\n2) Provide cheap beer to raise spirits\n3) Increase state benefits and universal credit to improve the standard of living\n'))
+    except ValueError:
+        print("Please enter a number.")
+        return Challenge1(title)
+
+    match choice2:
+        case 1:
+            print("Fantastic choice. The people are starting to realise that there are opportunities out there.\nAlthough there is still work to do, spirits are still low.")
+        case 2:
+            print("Well what a nice idea!!\nUnfortunately, the cheap booze caused a riot at the rugby.\n150 people died\nBUT spirits are still high! (Pun Intended)")
+        case 3:
+            print("Whatever are we paying you for?\nThe people have given up any kind of employment and are becoming couch potatoes.")
+        case _:
+            print("Invalid choice you gremlin. Try again")
+            return Challenge1(title)
 
 
 
